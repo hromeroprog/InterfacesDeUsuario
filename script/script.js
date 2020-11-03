76f16a4cf49df3c1490bb84f571b759620cf69a5
Marimf123 <71636280+Marimf123@users.noreply.github.com>
2020-11-03 23:51:39 +0100

    ya

--- script/script.js
+++ script/script.js
@@ -710,11 +710,9 @@ function getMonth(date) {
 //Modal Calendar
 $(document).ready(function() {
     // Get the modal
-    var modal = document.getElementById("myModal");
     var modal1 = document.getElementById("myModal1");
 
     // Get the button that opens the modal
-    var btn = document.getElementById("myBtn");
     var btn1 = document.getElementById("myBtn1");
 
     // Get the <span> element that closes the modal
@@ -722,17 +720,12 @@ $(document).ready(function() {
     var span1 = document.getElementsByClassName("close")[1];
 
     // When the user clicks the button, open the modal 
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
     btn1.onclick = function() {
         modal1.style.display = "block";
     }
 
     // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
+
     span1.onclick = function() {
         modal1.style.display = "none";
     }
@@ -740,10 +733,6 @@ $(document).ready(function() {
 
     // When the user clicks anywhere outside of the modal, close it
     window.addEventListener("click", function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-
         if (event.target == modal1) {
             modal1.style.display = "none";
 
